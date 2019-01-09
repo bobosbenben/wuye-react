872f253dd13048467b031c63b0896a86b1eca47b
syb <1208874645@qq.com>
2019-01-09 08:42:15 +0800

    测试git提交

--- src/component/BaoXiu.js
+++ src/component/BaoXiu.js
@@ -27,7 +27,7 @@ class BaoXiu extends Component {
             console.log('如果没有传入我的小区的id，也没有默认的小区，则页面跳转到新增我的小')
             //并且要添加路径参数，fromfaultreport的属性要设置为true，这样当设置完以后，就会跳转到保修页面
         }
-        fetch('http://192.168.2.45:8080/hello',{
+        fetch('http://192.168.2.126:8080/hello',{
             mode: "cors",
             method: 'post',
             headers: {
